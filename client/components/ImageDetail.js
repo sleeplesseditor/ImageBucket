61bb508489f1d5604e2c9b9bd6535c107610a24e
sleeplesseditor <josh@bryson.me.uk>
2018-03-13 15:19:28 +0000

    Bootstrap installed

--- client/components/ImageDetail.js
+++ client/components/ImageDetail.js
@@ -1,11 +1,18 @@
 //Image Detail
 import React from 'react';
 
-const ImageDetail = () => {
+const ImageDetail = (props) => {
     return (
-        <div>
-            Image Detail
-        </div>
+        <li className="media-list group-item">
+            <div className="media-left">
+                <img src={props.image.link} />
+            </div>
+            <div className="media-body">
+                <h4 className="media-heading">
+                    {props.image.title}
+                </h4>
+            </div>
+        </li>
     );
 };
 
--- client/components/ImageList.js
+++ client/components/ImageList.js
@@ -2,10 +2,20 @@
 import React from 'react';
 import ImageDetail from './ImageDetail';
 
+const IMAGES = [
+    { title: 'Pen', link: 'https://dummyimage.com/600x400' },
+    { title: 'Pine Tree', link: 'https://dummyimage.com/600x400' },
+    { title: 'Mug', link: 'https://dummyimage.com/600x400' },
+];
+
 const ImageList = () => {
+    const RenderedImages = IMAGES.map(function(image) {
+        return <ImageDetail image={image} />
+    });
+
     return (
-        <ul>
-            <ImageDetail />
+        <ul className="media-list list-group">
+            {RenderedImages}
         </ul>
     );
 };
